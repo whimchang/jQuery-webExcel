b79d7bdad8e1aae4fd5e025ab6d1fa1dc7dc50a7
whimchang <whimchang@163.com>
2014-10-11 16:43:58 +0800

    version0.0.2

--- jqueryExcel/WebContent/jQuery.Excel/js/jQuery.excelFucntions.js
+++ jqueryExcel/WebContent/jQuery.Excel/js/jQuery.excelFucntions.js
@@ -6,25 +6,31 @@
 				"cols":"8",			//type is number
 				"theams":"office",	//office
 				"title":"alpha",	//alpha,number
-				"edit":false,		//false,true
+				"creat":false,		//false,true
+				"edit":true,		//false,true
 				"export":false,		//false,ture
 				"dataType":"json",	//json,xml,array
-				"rowEdit":true,		//false,true
-				"colEdit":true,		//false,true
+				"rowEdit":false,		//false,true
+				"colEdit":false,		//false,true
+				"trDel":false,		//false,true
 				"sort":false,
 		};
 		this.options=$.extend({},this.defaults,opt);
 		this.alpha=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 		this.number=['0','1','2','3','4','5','6','7','8','9'];		
+		this.table;
 		this.caption;
 		this.trSet;
 		this.tdSet;
 		this.export_json='';
 		this.export_array;
+		this.sortIndex;
+		this.orderFlag=0;
 	};
 	functionClass.prototype={
-		//随机生成表格
+		//生成表格
 		tableGenerate:function(){	
+			var tit=this;
 			//表头  -- 名称和导出按钮
 			var headObj='<caption>tableTitle'
 						+'　　<a href="javascript:$.titleEdit();"><span class="glyphicon glyphicon-edit"></span></a>'
@@ -39,6 +45,7 @@
 			}
 			tableObj+='</table>';
 			this.$element.append(tableObj);
+			this.table=$("table");
 			this.caption=$("caption");
 			//表格tr单元集合
 			this.trSet=$("tr");
@@ -70,6 +77,43 @@
 			}
 			//编辑表格名称
 			$(headObj).tdEdit();
+			//删除一行
+			if(this.options.trDel){
+				$(this.trSet).each(function(key){
+					if(key>0){
+						tit.tableDeleteRow(this);
+					}
+				});
+			}
+			/*$.each(this.trSet,function(key,val){
+				if(key>0){
+					tit.tableDeleteRow(this);
+				}
+			});*/
+		},
+		
+		//新建表格
+		tableCreat:function(){
+			if(this.options.creat){
+				//alert("creat");
+				var tit=this;
+				$(this.caption).append('　　<span class="glyphicon glyphicon-plus" data-toggle="tooltip" title="New table"></span>');
+				$(this.caption).children('.glyphicon-plus').on({
+					"click":function(){
+						var r=confirm("该操作会丢失当前数据，您确定要新建表格吗？");
+						if (r==true){
+							//alert("creat");
+							window.location='http://localhost:8080/jqueryExcel/test.jsp';
+						}else{
+							//alert("not leave");
+						}
+					},
+					"mouseover":function(){
+						$(this).css({"cursor":"pointer"});
+						$(this).tooltip(); 
+					}
+				});
+			}
 		},
 		
 		//表格导出
@@ -121,17 +165,72 @@
 		//表格导入
 		tableImport:function(){
 			if(this.options.import){
-				alert("dsfdf");
+				//alert("dsfdf");
+				var tit=this;
 				$(this.caption).append('　　<span class="glyphicon glyphicon-import" data-toggle="tooltip" title="Import"></span>');
-				$(this.caption).children('.glyphicon-arrow-import').on();
+				$(this.caption).children('.glyphicon-import').on({
+					"click":function(){
+						//询问确认 是否导入表格
+						var r=confirm("该操作会丢失当前数据，您确定需要导入表格吗");
+						if(r==true){
+							//导入表格
+							$.ajax({
+								url:'./data/local/data_test.txt',//这里是由后台给出的json数据存储地址
+								type:'GET',
+								success:function(data){
+									//console.log(data);
+									var jsObj=eval('('+data+')');
+									//判断现有表格行和列是否满足将要导入的表格数据的要求
+									var trNum=Object.keys(jsObj).length;
+									$.each(jsObj,function(keyTr,val){
+										var rowIndex=parseInt(keyTr.charAt(keyTr.length-1));
+										var tdNum=val.length;	
+										if(tdNum!=tit.options.cols || trNum!=tit.options.rows){
+											$(tit.table).remove();
+											//重新生成新的表格
+											tit.options.cols=tdNum;
+											tit.options.rows=trNum;
+											tit.tableGenerate();
+											tit.tableExport();
+											tit.tableImport();
+											tit.tableSet();
+											tit.tableSort();
+											tit.tableCreat();
+											//写入表格内容
+											$.each(val,function(keyTd,value){
+												$(tit.tdSet[rowIndex*(tdNum+1)+(keyTd+1)]).text(value);
+												//console.log(rowIndex+' : '+value);
+											});
+										}else{
+											$.each(val,function(keyTd,value){
+												$(tit.tdSet[rowIndex*(tdNum+1)+(keyTd+1)]).text(value);
+												//console.log(rowIndex+' : '+value);
+											});	
+										}																										
+									});
+								},
+								error:function(){
+									console.log("get failed");
+								}
+							});
+						}else{
+							
+						}						
+					},
+					"mouseover":function(){
+						$(this).css({"cursor":"pointer"});
+						$(this).tooltip(); 
+					}
+				});
 			}
 		},		
 		
-		//增加or删除行和列		
+		//增加or删除一列 ;增加一行
 		tableSet:function(){
-			//增加一列
+			//增加一列 or 删除一列
 			if(this.options.rowEdit){
-				var tit=this;			
+				var tit=this;	
+				//增加一列
 				$(this.caption).append('　　<span class="glyphicon glyphicon-arrow-right" data-toggle="tooltip" title="Add Column"></span>');
 				$(this.caption).children('.glyphicon-arrow-right').on({
 					"click":function(){
@@ -148,22 +247,45 @@
 								$(this).append('<td></td>');
 								$(this).children("td").last().tdEdit();
 							}
-						});
+						});	
+						var index=tit.options.cols+1;
 						tit.options.cols+=1;
 						tit.tdSet=$("td");
 						tit.trSet=$("tr");
+						$(tit.trSet[0]).children('td').last().on({
+							"click":function(){
+								tit.tableSelectCol(index);
+							}
+						});							
 					},
 					"mouseenter":function(){
 						$(this).css({"cursor":"pointer",});
 						$(this).tooltip(); 
 					}
-				});				
+				});		
+				//删除一列
+				$(this.caption).append('　　<span class="glyphicon glyphicon-arrow-left" data-toggle="tooltip" title="Delete Column"></span>');
+				$(this.caption).children('.glyphicon-arrow-left').on({
+					"click":function(){
+						tit.trSet.each(function(){
+							$(this).children("td").last().remove();
+						});
+						tit.options.cols-=1;
+					},
+					"mouseenter":function(){
+						$(this).css({"cursor":"pointer",});
+						$(this).tooltip(); 
+					}
+				});
 			}else{
 				alert("您的设置不支持列编辑，请联系网站管理员");
 			}
+						
+			
 			//增加一行
 			if(this.options.colEdit){
-				var tit=this;			
+				var tit=this;		
+				//增加一行
 				$(this.caption).append('　　<span class="glyphicon glyphicon-arrow-down" data-toggle="tooltip" title="Add Row"></span>');
 				$(this.caption).children('.glyphicon-arrow-down').on({
 					"click":function(){
@@ -181,83 +303,134 @@
 						tit.options.rows+=1;
 						tit.tdSet=$("td");
 						tit.trSet=$("tr");
+						tit.tableDeleteRow($(tit.trSet).last());
 					},
 					"mouseenter":function(){
 						$(this).css({"cursor":"pointer",});
 						$(this).tooltip(); 
 					}
 				});
+				//tit.tableDeleteRow();
 			}else{
 				alert("您的设置不支持行编辑，请联系网站管理员");
-			}
-			
+			}			
 		},
 		
-		//表格排序 -- 按列
-		tableSort:function(){
+		
+		//删除一行  -- 每一行都有删除的链接
+		tableDeleteRow:function(ele){
 			var tit=this;
-			var tdFirstRow=$(this.trSet[0]).children('td');
+			var eletmp=ele;
+			//alert("delete row");
+			$(ele).children('td').first().on({
+				"click":function(){
+					var txt=$(eletmp).children('td').first().text();
+					//alert(txt);
+					$.each($(eletmp).nextAll(),function(key,val){
+						$(this).children('td').first().text(txt);
+						txt++;
+					});
+					$(eletmp).remove();					
+					tit.options.rows-=1;
+					tit.trSet=$("tr");
+					//调整左边列的显示序号
+					
+				},
+				"mouseenter":function(){
+					$(this).css({"cursor":"pointer"});
+					$(this).append('<span class="glyphicon glyphicon-remove"></span>');
+				},
+				"mouseleave":function(){
+					$(this).children('span').remove();
+				},
+			});		
+		},
+				
+		//表格排序 -- 按列
+		tableSort:function(){			
 			if(this.options.sort){
-				this.caption.append('　　<span class="glyphicon glyphicon-sort" data-toggle="tooltip" title="Sort"></span>');
-				$(this.caption).children(".glyphicon-sort").on({
-					"click":function(){
-						//alert("sort");
-						if($(".bgcolor").length==0 || $(".bgcolor").length>tit.options.rows+1){
-							alert("没有满足条件的排序内容");
-						}else{
-							//alert("sort");
-							
-						}
-					},
-					"mouseenter":function(){
-						$(this).css({"cursor":"pointer",});
-						$(this).tooltip(); 
-					},
-				});
-				//选中一列
+				var tit=this;			
+				var tdFirstRow=$(this.trSet[0]).children('td');
+				//选中需要排序的一列
 				tdFirstRow.each(function(){
 					if($(this).index()>0){
 						$(this).on({
 							"click":function(){
-								var index=$(this).index();
-								$(tit.tdSet).each(function(){
-									if($(this).index()==index){
-										$(this).toggleClass("bgcolor");
-									}
-								});								
-							},							
+								tit.sortIndex=$(this).index();
+								tit.tableSelectCol(tit.sortIndex);
+								//console.log(tit.sortIndex);
+							}						
 						});
 					};					
 				});//end 选中一列
+				var orderFlag=0;
+				this.caption.append('　　<span class="glyphicon glyphicon-sort" data-toggle="tooltip" title="Sort"></span>');
+				$(this.caption).children(".glyphicon-sort").on({
+					"click":function(){						
+						if($(".bgcolor").length==0 || $(".bgcolor").length>tit.options.rows+1){
+							alert("没有满足条件的排序内容");
+						}else{
+							var tdVal=[];
+							$.each(tit.tdSet,function(key,val){
+								if($(this).index()==tit.sortIndex && $(this).text()!=null){
+									tdVal[key]=$(this).text();
+								}
+							});
+							$.each(tit.trSet,function(key,val){
+								if(key>0){
+									$(this).attr('flag',tdVal[key*(tit.options.cols+1)+tit.sortIndex]);
+								}								
+							});
+							if(orderFlag==0){//降序
+								for(var j=0;j<tit.options.rows;j++){
+									for(var i=1;i<tit.options.rows-j;i++){									
+										if($(tit.trSet[i+1]).attr('flag')>$(tit.trSet[i]).attr('flag')){
+											$(tit.trSet[i-1]).after(tit.trSet[i+1]);
+											tit.trSet=$("tr");
+											tit.tdSet=$("td");
+										}
+									}									
+								}
+								orderFlag=1;
+							}else{//升序
+								for(var j=0;j<tit.options.rows;j++){
+									for(var i=1;i<tit.options.rows-j;i++){									
+										if($(tit.trSet[i+1]).attr('flag')<$(tit.trSet[i]).attr('flag')){
+											$(tit.trSet[i-1]).after(tit.trSet[i+1]);
+											tit.trSet=$("tr");
+											tit.tdSet=$("td");
+										}
+									}									
+								}
+								orderFlag=0;
+							}
+							//排序结束，重新设置左边一栏的显示序号
+							$.each(tit.trSet,function(key,val){
+								if(key>0){
+									$(this).children('td').first().text(key);
+								}
+							});
+						}
+					},
+					"mouseenter":function(){
+						$(this).css({"cursor":"pointer",});
+						$(this).tooltip(); 
+					}
+				});				
 			}
 		},		
 
-		//新建表格
-		tableCreat:function(){
-			if(this.options.creat){
-				//alert("creat");
-				var tit=this;
-				$(this.caption).append('　　<span class="glyphicon glyphicon-plus" data-toggle="tooltip" title="New table"></span>');
-				$(this.caption).children('.glyphicon-plus').on({
-					"click":function(){
-						var r=confirm("该操作会丢失当前数据，您确定要新建表格吗？");
-						if (r==true){
-							//alert("creat");
-							window.location='http://localhost:8080/webExcel/test.jsp';
-						}else{
-							//alert("not leave");
-						}
-					},
-					"mouseover":function(){
-						$(this).css({"cursor":"pointer"});
-						$(this).tooltip(); 
-					}
-				});
-			}
+		//选中一列
+		tableSelectCol:function(colIndex){
+			$.each(this.tdSet,function(key,val){
+				if($(this).index()==colIndex){
+					$(this).toggleClass("bgcolor");
+				}
+			});
 		},
 	};	
 	
-	//插件 -- 随机生成表格
+	//插件 -- jQuery webExcel
 	$.fn.tableGenerate=function(options){
 		var functionSets=new functionClass(this,options);
 		//return functionSets.tableGenerate();
@@ -267,13 +440,9 @@
 		functionSets.tableSet();
 		functionSets.tableSort();
 		functionSets.tableCreat();
+		return;
 	};
 	
-	//插件  -- 导出表格成为json格式
-	$.fn.tableExport=function(options){
-		var functionSets=new functionClass(this,options);
-		functionSets.tableExport();		
-	};
 	
 	//可编辑表格单元
 	$.fn.tdEdit=function(){
@@ -312,20 +481,4 @@
 			}
 		});
 	};
-	//选中一列
-	$.fn.selectCol=function(colId){
-		
-	};
-
-	//DOM元素绑定事件集合
-	$.extend({
-		//编辑表格名称
-		titleEdit:function(){			
-			alert("title edit");
-		},
-		//导出表格
-		/*tableExport:function(){
-			//alert("table export");
-		},*/
-	});
 })(jQuery,window,document);
--- jqueryExcel/WebContent/test.jsp
+++ jqueryExcel/WebContent/test.jsp
@@ -13,7 +13,7 @@
 <link rel="stylesheet" href="./jQuery.Excel/theams/office/office.css">
 </head>
 <body>
-<div id="table" class="container ui-widget-content" style="margin-top:50px;padding:50px;width:90%;"></div>
+<div id="table" class="container ui-widget-content" style="margin-top:100px;padding:50px;width:90%;"></div>
 
 <script type="text/javascript" src="./js/jquery-1.11.1.js"></script>
 <script type="text/javascript" src="./js/jquery-ui.js"></script>
@@ -22,16 +22,19 @@
 <script type="text/javascript">
 $(document).ready(function(){
 	$("#table").tableGenerate({
-		"rows":6,
-		"cols":8,
-		"title":"alpha",
-		"edit":true,
-		"export":true,
-		"import":true,
-		"sort":true,
-		"dataType":"json",
-		"path":"C:/Users/stock/git/jQuery-webExcel/webExcel/data",
-		"creat":true,
+		"rows":3,			//设置行数,默认值为5
+		"cols":4,			//设置列数,默认值为8
+		"title":"alpha",	//head格式,默认显示为alpha
+		"edit":true,		//可编辑,默认为true
+		"creat":true, 		//新建表格,默认为false
+		"export":true,		//导出,默认为false
+		"import":true,		//导入,默认为false
+		"rowEdit":true,		//增加行,默认为false
+		"trDel":true,		//删除行,默认为false
+		"colEdit":true,		//增加和删除列,默认为false
+		"sort":true,		//排序,默认为false
+		"dataType":"json",	//导出数据格式
+		"path":"C:/Disk/Software/Enviroment/apache-tomcat-6.0.41/jqueryExcel/WebContent/data/up", //json文件保存路径		
 	});
 	$("#table").draggable();
 });
